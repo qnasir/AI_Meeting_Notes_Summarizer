cf794fad31f589c24ef92a3c8eed984724795657
qnasir <qnasir575.com>
2025-08-16 16:28:20 +0530

    Created models

---
+++ server/models/Summary.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const SummarySchema = new mongoose.Schema({
+  transcript: {
+    type: String,
+    required: true
+  },
+  prompt: {
+    type: String,
+    required: false
+  },
+  summary: {
+    type: String,
+    required: true
+  },
+  editedSummary: {
+    type: String,
+    required: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Summary', SummarySchema);
