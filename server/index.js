eeeb025cc7d43aeb5e01e612ab502951ca8f1e52
qnasir <qnasir575.com>
2025-08-16 16:19:31 +0530

    Connected database

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const connectDB = require('./db');
 
 const app = express();
+
+// Connect to MongoDB
+connectDB();
+
 
 // Middleware
 app.use(cors());
