7ea8e5b286267f0020f1db69443ac223609994e1
qnasir <qnasir575.com>
2025-08-16 14:41:16 +0530

    Created homepage

--- .vite/deps/@tanstack_react-query.js
+++ .vite/deps/@tanstack_react-query.js
@@ -7,7 +7,7 @@ import {
   __privateWrapper,
   __toESM,
   require_react
-} from "./chunk-TJW4JA7A.js";
+} from "./chunk-ZCND2HWC.js";
 
 // node_modules/react/cjs/react-jsx-runtime.development.js
 var require_react_jsx_runtime_development = __commonJS({
--- .vite/deps/react.js
+++ .vite/deps/react.js
@@ -1,5 +1,5 @@
 import {
   require_react
-} from "./chunk-TJW4JA7A.js";
+} from "./chunk-ZCND2HWC.js";
 export default require_react();
 //# sourceMappingURL=react.js.map
